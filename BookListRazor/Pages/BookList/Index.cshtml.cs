62615b6994bee3257b58f4e7ba51dc4d2c410f81
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-11-12 02:15:51 +0400

    BookList's Index page designed ✅

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _context.Books.ToListAsync();
+        }
+    }
+}
