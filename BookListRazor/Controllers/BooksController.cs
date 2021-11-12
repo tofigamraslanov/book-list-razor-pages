85c498239c6eae75b96c3a41b0780123f44e711f
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-11-12 20:02:59 +0400

    Deleting book from DataTable and showing toastr notification implemented ✅

--- BookListRazor/Controllers/BooksController.cs
+++ BookListRazor/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
-﻿using System.Linq;
-using BookListRazor.Model;
+﻿using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace BookListRazor.Controllers
 {
@@ -16,9 +17,23 @@ namespace BookListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAllAsync()
         {
-            return Json(new { data = _context.Books.ToList() });
+            return Json(new { data = await _context.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var bookFromDb = await _context.Books.FindAsync(id);
+
+            if (bookFromDb == null)
+                return Json(new { success = false, message = "Error while deleting" });
+
+            _context.Books.Remove(bookFromDb);
+            await _context.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -20,12 +20,12 @@ function loadDataTable() {
                 "render": function(data) {
                     return `<div class="text-center">
                                 <a href="/BookList/Edit?id=${data
-                                    }" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        }" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                                     Edit
                                 </a>
                                 &nbsp;
                                 <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
-                                    onclick=Delete('/api/book?id='+${data})>
+                                    onclick=Delete('/api/books?id='+${data})>
                                     Delete
                                 </a>
                             </div>`;
@@ -37,5 +37,30 @@ function loadDataTable() {
             "emptyTable": "no data found"
         },
         "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function(data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
